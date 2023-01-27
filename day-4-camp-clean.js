c59507c34df7dc36bbddbdca1c2d8756d0ecba97
NadyaCodes <nadyacorscadden@gmail.com>
2023-01-27 13:16:49 -0500

    day 4 part 2

--- day-4-camp-clean.js
+++ day-4-camp-clean.js
@@ -14,6 +14,27 @@ const isOverlapping = (arr1, arr2) => {
   return false;
 };
 
+const isPartiallyOverlapping = (arr1, arr2) => {
+  const firstArray = [];
+  const secondArray = [];
+  let overlap = false;
+
+  for (let i = arr1[0]; i <= arr1[1]; i++) {
+    firstArray.push(i);
+  }
+
+  for (let i = arr2[0]; i <= arr2[1]; i++) {
+    secondArray.push(i);
+  }
+
+  secondArray.forEach((arrNum) => {
+    if (firstArray.includes(arrNum)) {
+      overlap = true;
+    }
+  });
+  return overlap;
+};
+
 fs.readFile("input-day-4-camp.txt", "utf-8", function (err, data) {
   const campArray = data.split("\n");
 
@@ -30,4 +51,19 @@ fs.readFile("input-day-4-camp.txt", "utf-8", function (err, data) {
   });
 
   console.log(totalOverlaps.length);
+
+  //part 2
+
+  const totalPartialOverlaps = [];
+
+  campArray.forEach((camp) => {
+    const pairs = camp.split(",");
+
+    const firstRange = convertNumber(pairs[0].split("-"));
+    const secondRange = convertNumber(pairs[1].split("-"));
+    if (isPartiallyOverlapping(firstRange, secondRange)) {
+      totalPartialOverlaps.push(camp);
+    }
+  });
+  console.log(totalPartialOverlaps.length);
 });
