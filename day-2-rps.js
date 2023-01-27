9e56d8218580b5501a06c30abc28c9b06465c282
NadyaCodes <nadyacorscadden@gmail.com>
2023-01-27 09:05:46 -0500

    day 2 part 2 solution

--- day-2-rps.js
+++ day-2-rps.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 fs.readFile("input-day-2-rps.txt", "utf8", function (err, data) {
   const rawData = data.split("\n");
-  let score = 0;
+  let score1 = 0;
 
   const findNum = (value) => {
     if (value === "X" || value === "A") {
@@ -19,27 +19,68 @@ fs.readFile("input-day-2-rps.txt", "utf8", function (err, data) {
   for (let i = 0; i < rawData.length; i++) {
     let elfNum = findNum(rawData[i][0]);
     let myNum = findNum(rawData[i][2]);
-    score += myNum;
+    score1 += myNum;
 
     //if answer is the same
     if (elfNum === myNum) {
-      score += 3;
+      score1 += 3;
     }
 
     //if elf has rock, paper wins...scissors wouldn't add
     if (elfNum === 1 && myNum === 2) {
-      score += 6;
+      score1 += 6;
     }
 
     //if elf has paper, scissors win...rock wouldn't add
     if (elfNum === 2 && myNum === 3) {
-      score += 6;
+      score1 += 6;
     }
 
     //if elf has scissors, rock wins...paper wouldn't add
     if (elfNum === 3 && myNum === 1) {
-      score += 6;
+      score1 += 6;
     }
   }
-  console.log(score);
+  console.log(score1);
+
+  //PART 2
+
+  let score2 = 0;
+  for (let i = 0; i < rawData.length; i++) {
+    let elfNum = findNum(rawData[i][0]);
+
+    const winMap = { 1: 2, 2: 3, 3: 1 };
+    const loseMap = { 1: 3, 2: 1, 3: 2 };
+
+    if (rawData[i][2] === "X") {
+      myNum = loseMap[elfNum];
+    }
+
+    if (rawData[i][2] === "Y") {
+      myNum = elfNum;
+    }
+
+    if (rawData[i][2] === "Z") {
+      myNum = winMap[elfNum];
+    }
+
+    score2 += myNum;
+
+    if (elfNum === myNum) {
+      score2 += 3;
+    }
+
+    if (elfNum === 1 && myNum === 2) {
+      score2 += 6;
+    }
+
+    if (elfNum === 2 && myNum === 3) {
+      score2 += 6;
+    }
+
+    if (elfNum === 3 && myNum === 1) {
+      score2 += 6;
+    }
+  }
+  console.log(score2);
 });
